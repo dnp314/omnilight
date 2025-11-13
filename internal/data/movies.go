02132ac72b0aea9477b675a2cb04dfbc3ff07727
div <dcparikh360@gmail.com>
2025-11-13 10:54:50 +0900

    initial commit

---
+++ internal/data/movies.go
@@ -0,0 +1,18 @@
+package data
+
+import (
+	"time"
+)
+
+// NOTE: The third column is for controlling how the key
+// appears in JSON-encoded format
+// - for not including, omitempty for showing only if not empty
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"-"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year,omitempty"`
+	Runtime   int32     `json:"runtime,omitempty"`
+	Genres    []string  `json:"genres,omitempty"`
+	Version   int32     `json:"version"`
+}
